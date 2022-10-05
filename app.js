2aa3e86f4af6b1ff9017568562b75836f39ee6da
Kyle DAmato <kyledamato4@gmail.com>
2022-10-05 15:26:10 -0400

    finished assignment

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const http = require('http'),
-path = require('path'),
-express = require('express'),
-bodyParser = require('body-parser');
+    path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 app.use(express.static('.'))
-app.use(bodyPardser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
 const db = new sqlite3.Database(':memory:');
@@ -18,4 +18,27 @@ app.get('/', function (req, res) {
     res.sendFile('index.html');
 });
 
-//test
+app.post('/login', function (req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	var query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
+
+	console.log("username: " + username);
+	console.log("password: " + password);
+	console.log("query: " + query);
+
+	db.get(query, function (err, row) {
+
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+	});
+
+});
+
+app.listen(3000);
