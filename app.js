0f1b9a13d61b7c6d1270e976980c5dbe279d3fc7
Kyle DAmato <kyledamato4@gmail.com>
2022-10-05 14:26:05 -0400

    linking to github repository

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http'),
+path = require('path'),
+express = require('express'),
+bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+app.use(express.static('.'))
+app.use(bodyPardser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function() {
+    db.run("CREATE TABLE user (username TEXT, password TEXT title TEXT)");
+    db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
+});
